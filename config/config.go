a9f23a7d367d8b363ca74990205ba90b9b44d6b2
mahfuzan <mahfuzan.hadi@sirclo.com>
2023-09-08 09:08:42 +0700

    fix typo

--- config/config.go
+++ config/config.go
@@ -10,12 +10,12 @@ type Config struct {
 	AmqpHost     string `envconfig:"amqp_host" default:""`
 	AmqpPort     string `envconfig:"amqp_port" default:""`
 	AmqpUser     string `envconfig:"amqp_user" default:""`
-	AmqpPassword string `envconfig:"amqp_user" default:""`
-	AmqpVhost    string `envconfig:"amqp_user" default:"\\"`
+	AmqpPassword string `envconfig:"amqp_password" default:""`
+	AmqpVhost    string `envconfig:"amqp_vhost" default:"\\"`
 
 	OmsNewOrderQueue string `envconfig:"oms_new_order_queue" default:"swiftoms.order-queue.new"`
 
-	OmsHost  string `envconfig:"oms_token" default:""`
+	OmsHost  string `envconfig:"oms_host" default:""`
 	OmsToken string `envconfig:"oms_token" default:""`
 }
 
